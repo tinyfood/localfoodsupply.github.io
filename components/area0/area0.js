0d964316b8c207a5153aaa05adaf640f76c588fb
G. Willson <greg@biomassiv.es>
2015-08-27 20:50:03 -0600

    Update area0.js

--- components/area0/area0.js
+++ components/area0/area0.js
@@ -1,4 +1,5 @@
 app.controller('Area0Controller', function () {
     console.log('Area0Controller');
-    this.name = 'The Origin of a Network';
+    this.name = 'The Origin of a Network';    
+    this.content = 'what we do we must do well. - some poet.';
 });
