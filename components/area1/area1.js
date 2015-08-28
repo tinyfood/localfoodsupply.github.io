a913003fbd80ed969cfe43035943b02d84fa9e6e
G. Willson <greg@biomassiv.es>
2015-08-27 21:55:26 -0600

    Update area1.js

--- components/area1/area1.js
+++ components/area1/area1.js
@@ -1,6 +1,6 @@
 app.controller('Area1Controller', function () {
     console.log('Area1Controller');
     this.name = 'Our Food Future';
-    this.content = 'Its interesting to think about how things work in Australia vs. here, but from the first video I watched:     I was thinking that they created this network because the \'food desert\' effect was strongly limiting people\'s access to quality food.  - So they formed a really amazing network, complete with local organizing areas, and templates for new hubs. This allowed for new food distribution businesses to succeed.  This provided more confidence for the local farmer to plan for the needs of her expanded market, ... a healthy local economy had developed around the new system, as well as the core goal- better nutrition, more local food, local awareness of local ercological issues, like pollinator health.   ';
+    this.content = 'They created a network because the \'food desert\' effect was strongly limiting people\'s access to quality food.  - So they formed a really amazing network, complete with local organizing areas, and templates for new hubs. This allowed for new food distribution businesses to succeed.  This provided more confidence for the local farmer to plan for the needs of her expanded market, ... a healthy local economy had developed around the new system, as well as the core goal- better nutrition, more local food, local awareness of local ercological issues, like pollinator health.   ';
     this.author = 'Greg';
 });
