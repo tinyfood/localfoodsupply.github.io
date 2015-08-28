1b69620568111df272413d689ca07cb087de5d5a
G. Willson <greg@biomassiv.es>
2015-08-28 11:56:02 -0600

    Create area4.js

---
+++ components/area3/area4.js
@@ -0,0 +1,5 @@
+app.controller('Area4Controller', function () {
+    console.log('Area4Controller');
+    this.name = 'Planning for the future of local sustainable agriculture.';
+    this.content = 'To be a strong network resource to help connect people for planning via options for providing for markets in the mountains. Better tools allow us to plan budgets based on expectations of the market by seeing early signups and regular buyers in our network expressing support, the next season could be bountiful.';
+});
