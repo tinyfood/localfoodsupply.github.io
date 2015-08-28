558261079b28ec8ae84ad89f52e4049b0bf1bc7b
G. Willson <greg@biomassiv.es>
2015-08-27 18:48:50 -0600

    Update area4.js

--- components/area4/area4.js
+++ components/area4/area4.js
@@ -1,1 +1,5 @@
-
+app.controller('Area2Controller', function () {
+    console.log('Area2Controller');
+    this.name = 'What we are as a food network, and what we want to become.';
+    this.content = 'We are currently a group of friends, some of whom keep chickens and sell eggs, some of whom are involved in grassfed organic beef, and llama fur production, others among us are info urban aquaculture and specialty herbs- basil, microgreens, who are you?';
+});
